602a4a180678f2839974b573465fc7c779e9495c
Mohamed Sallam <m7.sallam@gmail.com>
2026-02-13 19:33:34 +0300

    Flattened structure to standard 2-layer Frappe layout

--- drawing_analyzer/drawing_analyzer/drawing_analyzer/hooks.py
+++
@@ -1,6 +0,0 @@
-app_name = "drawing_analyzer"
-app_title = "Drawing Analyzer"
-app_publisher = "Mohamed Sallam"
-app_description = "AI Area Calculator for 2D Drawings"
-app_email = "m7.sallam@gmail.com"
-app_license = "mit"
