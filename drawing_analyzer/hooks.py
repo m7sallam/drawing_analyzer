b40e9d8327ca660369e180748c6e0d4549f09b99
Mohamed Sallam <m7.sallam@gmail.com>
2026-02-13 07:24:11 +0300

    First engineering build

---
+++ drawing_analyzer/hooks.py
@@ -0,0 +1,6 @@
+app_name = "drawing_analyzer"
+app_title = "Drawing Analyzer"
+app_publisher = "Mohamed Sallam"
+app_description = "AI Area Calculator for 2D Drawings"
+app_email = "m7.sallam@gmail.com"
+app_license = "mit"
